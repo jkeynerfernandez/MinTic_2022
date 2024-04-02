e8ca227b7884b356cefe7a2ac93aa7e40a95f51c
jkeynerfernandez <fernandezdj54@gmail.com>
2024-04-01 21:44:49 -0500

    tiene errores

---
+++ prueba_keyner/MvcKeyner/Data/CrudKeynerContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MvcKeyner.Models;
+
+namespace MvcKeyner.Data{
+
+    public class CrudKeynerContext : DbContext{
+        
+        public CrudKeynerContext(DbContextOptions<CrudKeynerContext> options) : base(options){
+
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+
+}
--- prueba_keyner/mvcKeyner/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace mvcKeyner.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
